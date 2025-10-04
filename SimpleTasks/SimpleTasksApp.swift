88801c8272d3457c2506dc550445d6b5d845415d
Cody Tate <codyq.tate@gmail.com>
2025-10-04 13:58:13 -0400

    Initial Commit

---
+++ SimpleTasks/SimpleTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTasksApp.swift
+//  SimpleTasks
+//
+//  Created by Cody Tate on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleTasksTests/SimpleTasksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTasksTests.swift
+//  SimpleTasksTests
+//
+//  Created by Cody Tate on 10/4/25.
+//
+
+import Testing
+@testable import SimpleTasks
+
+struct SimpleTasksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
